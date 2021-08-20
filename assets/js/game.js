90d1e2b8a5b4195eec38d6793b0d7b0884e547e5
Te Wei <tw2159@nyu.edu>
2021-08-19 18:08:40 -0700

    fix prompt bugs

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,12 @@ var fight = function(enemy) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
+    // Conditional Recursive Function Call
+    if (promptFight === "" || promptFight === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+      return fightOrSkip();
+    }
+
     // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
       // confirm player wants to skip
@@ -23,13 +29,13 @@ var fight = function(enemy) {
         window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerInfo.money for skipping
         playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
-      }
+        return true;
     }
 
-    // generate random damage value based on player's attack power
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+    // repeat and execute as long as the enemy-robot is alive 
+    while (playerInfo.health > 0 && enemy.health > 0) {
+      fightOrSkip(); // <-- Replace code with this function call
+      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
